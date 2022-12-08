4eee54a85d10946ce5bc4f7779b60c6a9f8d8151
Adam <adamdtaylor2005@gmail.com>
2022-12-07 23:20:29 -0600

    Add files via upload

---
+++ src/pages/car_info.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Hero from "../components/hero.jsx";
+import Sections from "../components/sections.jsx";
+import CarInfo from "../components/car_info.jsx";
+import "../styles/sections.css";
+import "../styles/car_info.css";
+import DeveloperInfo from "../components/developer_info.jsx";
+// about us page
+
+export default function CarInfoPage() {
+  
+  return (
+    <>
+    <CarInfo />
+    </>
+  );
+}
