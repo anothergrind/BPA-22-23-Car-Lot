9d49c528a1f428e85c0284397b4d36af4ab0bd51
RedGrind <97067525+RedGrind@users.noreply.github.com>
2022-12-10 12:52:33 -0600

    Update and rename footer.jsx to Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../styles/footer.css";
+import "../styles/Footer.css";
 import wolf from "../pictures/weiss_wolves.png";
 
 // component for footer used in every page on the website
@@ -10,12 +10,8 @@ const Footer = () => {
       {/* company description section */}
       <div className="footer-container">
         <h1>Wolves Dealership<sup>©</sup></h1>
-        <p id="description"> 'Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged.'</p>
+        <p id="description"> Wolves Dealership is an online car business that provides a convenient and hassle-free way for you to purchase your dream car. We offer a wide selection of quality new and used vehicles from all the major brands. Our dealership aims to provide people with high-quality cars at an affordable price that users agree with.
+        </p>
       </div>
       {/* company logo */}
       <img id="footer-wolves-icon" src={wolf} alt="weiss-logo" />
@@ -38,8 +34,8 @@ const Footer = () => {
       {/* Unordered list of contacts users can reach out to */}
       <div className="contacts">
         <h1>Contacts</h1>
-        <h4>    : Phone Number +1 512 594 1400</h4>
-        <h4>    : 5201 Wolf Pack Dr, Pflugerville, TX, 78660</h4>
+        <h4>Phone Number: +1 512 594 1400</h4>
+        <h4>Address: 5201 Wolf Pack Dr, Pflugerville, TX, 78660</h4>
       </div>
     </div>
   );
