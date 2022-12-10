42a88fcc3b823fc3a5c14038f254b09aae7dbfdd
RedGrind <97067525+RedGrind@users.noreply.github.com>
2022-12-10 12:57:18 -0600

    Update and rename car_info.js to CarInfo.js

---
+++ src/pages/CarInfo.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Hero from "../components/Hero.jsx";
+import Sections from "../components/Sections.jsx";
+import CarInfo from "../components/CarInfo.jsx";
+import "../styles/Sections.css";
+import "../styles/CarInfo.css";
+import DeveloperInfo from "../components/DeveloperInfo.jsx";
+
+export default function CarInfoPage() {
+  return (
+    <>
+    <CarInfo />
+    </>
+  );
+}
