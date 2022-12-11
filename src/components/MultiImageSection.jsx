011b4465cc2c0c99d75fe198ceb08480e42d0278
RedGrind <97067525+RedGrind@users.noreply.github.com>
2022-12-11 15:35:05 -0600

    Update MultiImageSection.jsx

--- src/components/MultiImageSection.jsx
+++ src/components/MultiImageSection.jsx
@@ -10,20 +10,20 @@ export default function MultiSection(props) {
         <img
           id="image-1"
           className="reviewer-image"
-          src={props.image_url1}
-          alt="temp"
+          src={props.image_url3}
+          alt="Black woman and mixed boy looking at the same laptop"
         />
         <img
         id="image-2"
         className="reviewer-image"
-        src={props.image_url2}
-        alt="temp" 
+        src={props.image_url4}
+        alt="A family looking at the same laptop" 
         />
         <img
         id="image-3"
         className="reviewer-image"
-        src={props.image_url3}
-        alt="temp"
+        src={props.image_url5}
+        alt="Middle aged white lady searching for cars on her laptop"
         />
       </div>
     </div>
