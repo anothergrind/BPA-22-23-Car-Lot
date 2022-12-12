e9fc46845ba9519bde387164e95295eeb248f867
Adam <adamdtaylor2005@gmail.com>
2022-12-11 21:41:51 -0600

    Update CarInfo.jsx

--- src/components/CarInfo.jsx
+++ src/components/CarInfo.jsx
@@ -1,46 +1,47 @@
 import React from "react";
-import Sections from "./Sections.jsx";
-import SectionContainer from "./SectionContainer.jsx";
-import "../styles/sections.css";
-import "../styles/car_info.css";
+import "../styles/Sections.css";
+import "../styles/CarInfo.css";
 
 // about us page
+function Info(props) {
+    return ( 
+    <div className="info">
+        <h1>{props.tag1}: {props.value1}</h1>
+        <h1>{props.tag2}: {props.value2}</h1>
+    </div>
+    )
+}
 
-export default function CarInfo() {
-
-    function Info(props) {
-        return ( 
-        <div className="info">
-            <h1>{props.tag1}</h1>
-            <h1>{props.tag2}</h1>
-        </div>
-        )
-    }
-
-
+export default function CarInfo(props) {
   return (
     <body style={{backgroundColor: "brown"}}>
-        < img className="car-image-slide" src = "https://uploads.codesandbox.io/uploads/user/4c2a8a2f-dad6-413e-bf24-18c8e11392c7/IKk0-ea6c6ac0563e321f006761b13f2caf24.png" />
+        <img className="car-image-slide" src = {props.image_url} />
         <div className="section-container" id="specs-container">
             <div className="extra-images">
-                <img className="extra-image" src= "https://uploads.codesandbox.io/uploads/user/4c2a8a2f-dad6-413e-bf24-18c8e11392c7/IKk0-ea6c6ac0563e321f006761b13f2caf24.png" />
-                <img className="extra-image" src= "https://uploads.codesandbox.io/uploads/user/4c2a8a2f-dad6-413e-bf24-18c8e11392c7/IKk0-ea6c6ac0563e321f006761b13f2caf24.png" />
-                <img className="extra-image" src= "https://uploads.codesandbox.io/uploads/user/4c2a8a2f-dad6-413e-bf24-18c8e11392c7/IKk0-ea6c6ac0563e321f006761b13f2caf24.png" />
-                <img className="extra-image" src= "https://uploads.codesandbox.io/uploads/user/4c2a8a2f-dad6-413e-bf24-18c8e11392c7/IKk0-ea6c6ac0563e321f006761b13f2caf24.png" />
-                <img className="extra-image" src= "https://uploads.codesandbox.io/uploads/user/4c2a8a2f-dad6-413e-bf24-18c8e11392c7/IKk0-ea6c6ac0563e321f006761b13f2caf24.png" />
+                <img className="extra-image" src = {props.extra_images1} />
+                <img className="extra-image" src = {props.extra_images2} />
+                <img className="extra-image" src = {props.extra_images3} />
+                <img className="extra-image" src = {props.extra_images4} />
+                <img className="extra-image" src = {props.extra_images5} />
             </div>    
             <div className="info-rows">
                 <Info 
-                tag1 = "Model: temp"
+                tag1 = "Model"
+                value1 = {props.the_value1}
                 tag2 = "Make: temp"
+                value2 = {props.the_value2}
                 />
                 <Info 
-                tag1 = "Fuel Type: temp"
-                tag2 = "Cylinder: temp"
+                tag1 = "Fuel Type"
+                value1 = {props.the_value3}
+                tag2 = "Cylinder"
+                value2 = {props.the_value4}
                 />
                 <Info 
-                tag1 = "Year: 2020"
-                tag2 = "Condition: temp"
+                tag1 = "Year"
+                value1 = {props.the_value5}
+                tag2 = "Condition"
+                value2 = {props.the_value6}
                 />
             </div>
         </div>
