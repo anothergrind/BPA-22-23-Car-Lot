3231b975b98bc4d5f137a04912e463d8665813ba
Adam <adamdtaylor2005@gmail.com>
2023-02-06 14:16:06 -0600

    Update SearchSection.jsx

--- src/components/SearchSection.jsx
+++ src/components/SearchSection.jsx
@@ -8,17 +8,16 @@ import "../styles/Inventory.css";
 export default function SearchSection() {
   return (
     <div className="section-container" id="section-three">
-      <br/>
-      <h1>Start Shopping</h1>
-        <form className="search-bar" id="section-search">
+      <h1>Hit Inventory in the navbar to start shopping!</h1>
+        {/* <form className="search-bar" id="section-search">
           <input type="search" id="query" name="q" place  placeholder="Search..." />
           <a href="/inventory"> 
             <button id="click">Search</button>
-          </a>
-          <a href="/inventory">
+          </a> */}
+          {/* <a href="/inventory">
             <h4 id="filter">Advanced Filter</h4>
-          </a>
-        </form>
+          </a> */}
+        {/* </form> */}
     </div>
   );
 }
