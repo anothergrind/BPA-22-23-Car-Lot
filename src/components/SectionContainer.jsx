a72ca32a94a32a54f08eca81fab564314c346c05
Adam <adamdtaylor2005@gmail.com>
2023-02-06 14:16:31 -0600

    Update SectionContainer.jsx

--- src/components/SectionContainer.jsx
+++ src/components/SectionContainer.jsx
@@ -8,7 +8,7 @@ export default function SectionContainer(props) {
     <div className="section-container" id={props.container_id}>
       <h1 id="purpose-header">Our Purpose</h1>
       <div className="body-section" id={props.body_id}>
-        <h2>
+        <h2 id="container-h2">
           {props.body_text1}
           {props.body_text2}
         </h2>
