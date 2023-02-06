d754e21fdea0703bc5e22b930850d1e9a85d20fc
Adam <adamdtaylor2005@gmail.com>
2023-02-06 11:48:08 -0600

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,28 +1,30 @@
 import React, { useState } from "react";
 import "../styles/Modal.css";
 
-  export default function Modal() {
-    const [isOpen, setIsOpen] = useState(true);
-    const toggleModal = () => setIsOpen(!isOpen);
-    return (
+
+export default function Modal(props) {
+  const [isOpen, setIsOpen] = useState(true);
+  const toggleModal = () => setIsOpen(!isOpen);
+
+  return (
     <>
       {isOpen && (
         <div className="modal-wrapper">
           <div className="modal">
             <div className="modal-header">
-              <h2>Modal Title</h2>
+              <h2>{props.title}</h2>
               <button onClick={toggleModal}>Close</button>
             </div>
             <div className="modal-body">
               <div className="modal-left">
                 <img
-                  src="https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                  src={props.image}
                   alt="Modal Image"
                   className="img"
                 />
               </div>
               <div className="modal-right">
-                <p>Modal Content Here</p>
+                <p>{props.text}</p>
               </div>
             </div>
           </div>
