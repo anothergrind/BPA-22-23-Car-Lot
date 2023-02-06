5cf62ca98bb992e79c111288f9a21d85f1f469d9
Adam <adamdtaylor2005@gmail.com>
2023-02-06 14:17:34 -0600

    Update Sections.jsx

--- src/components/Sections.jsx
+++ src/components/Sections.jsx
@@ -6,14 +6,15 @@ export default function Sections(props) {
   return (
     <>
       <div className="section-container" id={props.id}>
-        <h1 id="header">{props.header}</h1>
-        <div className="theBody">
+        <h1 id={props.header_id}>{props.the_header}</h1>
+        <div id={props.body_id}className="theBody">
           <img
             className="image"
             src= {props.image_url}
+            id = {props.image_id}
             temp="Red Jeep on the top of a hill"
           />
-          <p id="para">
+          <p id={props.para_id}>
             {props.paragraph1}
             {props.paragraph2}
             {props.paragraph3}
@@ -22,6 +23,12 @@ export default function Sections(props) {
             {props.paragraph4}
             <br />
             {props.paragraph5}
+            <br />
+            <br />
+            {props.paragraph6}
+            <br />
+            <br />
+            {props.paragraph7}
           </p>
         </div>
       </div>
