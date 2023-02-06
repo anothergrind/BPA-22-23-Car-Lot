2f9201b426bcb45d1a56e281fafab057938344ac
Adam <adamdtaylor2005@gmail.com>
2023-02-06 14:14:10 -0600

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -12,19 +12,20 @@ export default function Modal(props) {
         <div className="modal-wrapper">
           <div className="modal">
             <div className="modal-header">
-              <h2>{props.title}</h2>
-              <button onClick={toggleModal}>Close</button>
+              <h1>{props.title}</h1>
+              <button className="modal-close" onClick={toggleModal}>Close</button>
             </div>
             <div className="modal-body">
               <div className="modal-left">
                 <img
                   src={props.image}
                   alt="Modal Image"
-                  className="img"
+                  className="modal-img"
+                  id={props.image_id}
                 />
               </div>
               <div className="modal-right">
-                <p>{props.text}</p>
+                <p className="modal-text">{props.text}</p>
               </div>
             </div>
           </div>
