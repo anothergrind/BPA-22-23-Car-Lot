2b514a61716525593464ed4f1e7cf21323d52af4
Adam <adamdtaylor2005@gmail.com>
2023-02-06 14:11:37 -0600

    Update CarContainer.jsx

--- src/components/CarContainer.jsx
+++ src/components/CarContainer.jsx
@@ -3,14 +3,14 @@ import "../styles/Inventory.css";
 import "../styles.css";
 
 // component for hero section used in the home page
-export default function Car_container(props) {
+export default function CarContainer(props) {
   return (
     <>
     <div className="car-container">
       <img className="car-image" src={props.image_url} alt="car-image" />
       <h1>Name: {props.name}</h1>
-      <h1>Price Range: {props.price_range}</h1>
-      <h1>Mileage: {props.mileage}</h1>
+      <h1>Price: ${props.price.toLocaleString("en-US")}</h1>
+      <h1>Mileage: {props.mileage.toLocaleString("en-US")} Mi.</h1>
     </div>
     </>
   );
